a3c971d89e74102c8db7ec7e2eb41dc6624b4074
Jordan Khamvongsouk <jordankhamvongsouk@gmail.com>
2021-04-05 20:30:35 -0600

    Add project files.

---
+++ Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assignment10.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<Bowlers> Bowlers { get; set; }
+
+        public PageNumberingInfo PageNumberingInfo { get; set; }
+
+        public string Team { get; set; }
+    }
+}
